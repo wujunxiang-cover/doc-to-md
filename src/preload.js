0b84d800abf79a19c246c3134fe31ea060183df7
wujunxiang-cover <wujunxiang-cover@users.noreply.github.com>
2026-09-22 20:15:34 +0800

    Remove retired desktop app files

--- src/preload.js
+++
@@ -1,6 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-
-contextBridge.exposeInMainWorld('docToMd', {
-  convert: (file) => ipcRenderer.invoke('convert-file', file),
-  save: (payload) => ipcRenderer.invoke('save-markdown', payload)
-});
